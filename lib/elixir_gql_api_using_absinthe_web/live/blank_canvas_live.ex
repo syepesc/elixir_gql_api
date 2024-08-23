6d6e931cd263b1a08909670d0eada1a8d95c7e93
Santiago Yepes <syepesc@hotmail.com>
2024-08-23 15:41:54 -0400

    Add initial blank_canvas_live route

---
+++ lib/elixir_gql_api_using_absinthe_web/live/blank_canvas_live.ex
@@ -0,0 +1,196 @@
+defmodule ElixirGqlApiUsingAbsintheWeb.BlankCanvasLive do
+  use ElixirGqlApiUsingAbsintheWeb, :live_view
+
+  @impl Phoenix.LiveView
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(page_title: "Home")
+
+    {:ok, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def render(assigns) do
+    ~H"""
+    <div class="flex flex-col gap-6 p-6">
+      <div class="border-2">
+        HOME
+      </div>
+      <div class="border-2">
+        HOME  HOME  HOME  HOME  HOME  HOMEHOMEHOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME  HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME HOME
+      </div>
+      <div class="flex flex-row flex-wrap">
+        <div class="border-2">
+          HOMEHOME
+        </div>
+        <div class="border-2">
+          HOMEHOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+        <div class="border-2">
+          HOME
+        </div>
+      </div>
+    </div>
+    """
+  end
+end
